75e7ee3611879f2c351abaec97b2c41ab05c9908
Anthony <anthonybruno63@yahoo.com>
2026-08-23 09:33:40 -0700

    wip vector

--- flight-computer/src/main.rs
+++ flight-computer/src/main.rs
@@ -7,13 +7,56 @@ use esp_idf_hal::units::Hertz;
 
 
 use mpu6050::Mpu6050;
+use mpu6050::device::ACCEL_HPF;
 use bmp280_ehal::{BMP280, Control, Oversampling, PowerMode};
 use shared_bus::BusManagerSimple;
+
+use nalgebra::{Vector3};
+
+
+const TIME_RATE: u32 = 100;
+
 
 fn calculate_altitude(p: f64) -> f64 {
     let center = p / 1013.25;
     let alt = 44330.77 * (1.0 - (center.powf(0.190263)));
     (alt * 1000.0).round() / 1000.0
+}
+
+
+fn g_to_mpss(acc_vec: Vector3<f32>) -> Vec<f32> {
+    let mut mpss_vec: Vec<f32> = Vec::new();
+    mpss_vec.push(acc_vec.x * 9.80665);
+    mpss_vec.push(acc_vec.y * 9.80665);
+    mpss_vec.push(acc_vec.z * 9.80665);
+    
+    mpss_vec
+}
+
+
+fn get_dist(accel: &Vec<f32>, x_old: f32, y_old: f32, z_old: f32) -> (f32, f32,f32) {
+    let dt = TIME_RATE as f32 / 1000.0;
+    let x_dist = x_old + (0.5 *  accel[0] * dt.powf(2.0) as f32);
+    let y_dist = y_old + (0.5 *  accel[1] * dt.powf(2.0) as f32);
+    let z_dist = z_old + (0.5 *  accel[2] * dt.powf(2.0) as f32);
+    log::info!("x_old: {:?} -- x_new: {:?} --- x_dist: {:?}", x_old, accel[0], x_dist);
+    (x_dist, y_dist, z_dist)
+}
+
+
+fn get_velocity(vel_o: f32, accel: f32) -> f32 {
+    vel_o + accel * (TIME_RATE as f32 / 1000.0)
+}
+
+fn calibrate_accel(mpu: &mut Mpu6050, samples: usize) -> Vector3<f32> {
+    let mut sum = Vector3::new(0.0f32, 0.0, 0.0);
+    for _ in 0..samples {
+        let a = mpu.get_acc().unwrap();
+        sum += a;
+        FreeRtos::delay_ms(10);
+    }
+    let avg = sum / samples as f32;
+    Vector3::new(avg.x, avg.y, avg.z - 1.0)
 }
 
 fn main() {
@@ -26,8 +69,6 @@ fn main() {
     let scl = peripherals.pins.gpio12;
 
 
-
-
     let config = I2cConfig::new().baudrate(Hertz(400_000));
     let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config).unwrap();
     
@@ -35,6 +76,9 @@ fn main() {
 
     let mut mpu = Mpu6050::new(bus.acquire_i2c());
     mpu.init(&mut FreeRtos).unwrap();
+    log::info!("Calibrating, keep sensor still...");
+    let bias = calibrate_accel(&mut mpu, 200); // ~2 seconds at 10ms delay
+    log::info!("Bias: {:?}", bias);
 
 
     let mut bmp = BMP280::new(bus.acquire_i2c()).unwrap();
@@ -46,19 +90,26 @@ fn main() {
         mode: PowerMode::Normal,
     });
 
+    let mut vx = 0.0;
+    let mut vy = 0.0;
+    let mut vz = 0.0;
 
     loop {
-        let accel = mpu.get_acc().unwrap();
-        let gyro = mpu.get_gyro().unwrap();
-        log::info!("\nAccel: x={:.2} y={:.2} z={:.2}", accel.x, accel.y, accel.z);
-
+        let raw = mpu.get_acc().unwrap();
+        let corrected = raw - bias;
+        let accel = g_to_mpss(corrected);
+        // let gyro = mpu.get_gyro().unwrap();
+        vx = get_velocity(vx, accel[0]);
+        vy = get_velocity(vy, accel[0]);
+        vz = get_velocity(vz, accel[0]);
+        log::info!("\nAx: {:?}\n Ay: {:?}\nAz: {:?}", accel[0], accel[1], accel[2]);
+        // log::info!("\nVx = {:?}\nVy = {:?}\n Vz = {:?}", vx,vy,vz);
 
         let pressure = bmp.pressure() / 100.0;
         let altitude = calculate_altitude(pressure);
         let bmp_temp = bmp.temp();
-        log::info!("\nBMP Temp: {:.2} C\nAltitude: {:?} m", bmp_temp, altitude);
+        // log::info!("\nBMP Temp: {:.2} C\nAltitude: {:?} m", bmp_temp, altitude);
 
-
-        FreeRtos::delay_ms(100);
+        FreeRtos::delay_ms(TIME_RATE);
     }
 }
